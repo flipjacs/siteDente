f319735bfdaef904471639bcf6ff5c3b51565c3b
Luiz Felipe Sousa Neilson <luizfelipeskype@gmail.com>
2026-03-11 16:31:07 -0400

    organização final

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const images = [
   "https://images.unsplash.com/photo-1588776814546-1ffcf47267a5",
   "img/sorriso.jpg",
-  "img/cirurgiaodentista.jpg"
+  "img/cirurgiaodentista.jpg",
 ];
 
 const carousel = document.querySelector(".carousel-bg");
 let index = 0;
 let interval;
 
-images.forEach(src => {
+images.forEach((src) => {
   const img = new Image();
   img.src = src;
 });
@@ -53,59 +53,59 @@ function prevSlide() {
 startCarousel();
 
 // animações sections
-const sections = document.querySelectorAll('section');
-function showSections(){
+const sections = document.querySelectorAll("section");
+function showSections() {
   const trigger = window.innerHeight * 0.85;
-  sections.forEach(sec=>{
-    if(sec.getBoundingClientRect().top < trigger){
-      sec.classList.add('show');
+  sections.forEach((sec) => {
+    if (sec.getBoundingClientRect().top < trigger) {
+      sec.classList.add("show");
     }
   });
 }
-window.addEventListener('scroll', showSections);
+window.addEventListener("scroll", showSections);
 showSections();
 
-const navLinks = document.querySelectorAll('nav a');
-const spySections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll("nav a");
+const spySections = document.querySelectorAll("section");
 
-window.addEventListener('scroll', () => {
-  let current = '';
-  spySections.forEach(section => {
+window.addEventListener("scroll", () => {
+  let current = "";
+  spySections.forEach((section) => {
     const sectionTop = section.offsetTop - 120;
     if (pageYOffset >= sectionTop) {
-      current = section.getAttribute('id');
+      current = section.getAttribute("id");
     }
   });
 
-    navLinks.forEach(link => {
-      link.classList.remove('active');
-      if (link.getAttribute('href') === '#' + current) {
-        link.classList.add('active');
-      }
-    });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
 });
 
-    // MENU MOBILE
-const toggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.nav-menu');
+// MENU MOBILE
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-menu");
 
-toggle.addEventListener('click', (e) => {
+toggle.addEventListener("click", (e) => {
   e.stopPropagation();
-  menu.classList.toggle('active');
+  menu.classList.toggle("active");
 });
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
   if (
-    menu.classList.contains('active') &&
+    menu.classList.contains("active") &&
     !menu.contains(e.target) &&
     !toggle.contains(e.target)
   ) {
-    menu.classList.remove('active');
+    menu.classList.remove("active");
   }
 });
 
-document.querySelectorAll('.nav-menu a').forEach(link => {
-  link.addEventListener('click', () => {
-    menu.classList.remove('active');
+document.querySelectorAll(".nav-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
   });
 });
